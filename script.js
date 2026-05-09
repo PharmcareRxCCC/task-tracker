476fc767b03d96a30168259ea3abbd6666c713b3
PharmcareRxCCC <pharmcarerxccc@gmail.com>
2026-05-09 15:05:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbwS2Vaw_yzUf0vTwu0G3E5Gd-V3TWAYUJEy0e1jlTnMf_zNdUyYb5EY1P5Exyvi09Ua/exec";
+<script>
 
-async function saveTask() {
+const SCRIPT_URL =
+"https://script.google.com/macros/s/AKfycbwS2Vaw_yzUf0vTwu0G3E5Gd-V3TWAYUJEy0e1jlTnMf_zNdUyYb5EY1P5Exyvi09Ua/exec";
+
+async function saveTask(){
 
   const taskDescription =
     document.getElementById("taskDescription").value;
@@ -9,16 +11,31 @@ async function saveTask() {
   const createdBy =
     document.getElementById("createdBy").value;
 
-  const response = await fetch(SCRIPT_URL, {
-    method: "POST",
-    body: JSON.stringify({
-      taskDescription,
-      createdBy
-    })
-  });
+  try{
 
-  const result = await response.json();
+    const response = await fetch(SCRIPT_URL,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        taskDescription,
+        createdBy
+      })
+    });
 
-  document.getElementById("result").innerHTML =
-    "Saved: " + result.taskId;
+    const result = await response.json();
+
+    document.getElementById("result").innerHTML =
+      "Saved Task: " + result.taskId;
+
+  }catch(error){
+
+    document.getElementById("result").innerHTML =
+      "Error saving task";
+
+    console.error(error);
+  }
 }
+
+</script>
